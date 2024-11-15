f84e435bb4c0bb295dd2bb44bcf3f38d3f9590d0
ridhoFebri <ridhofebriansyahsinsen@gmail.com>
2024-11-15 16:27:58 +0700

    update 1

--- test-ocr/tesseract/index.php
+++ test-ocr/tesseract/index.php
@@ -10,9 +10,9 @@ use thiagoalessio\TesseractOCR\TesseractOCR;
 // Tentukan path Tesseract di sini
 $tesseractPath = 'C:\ProgramData\chocolatey\lib\capture2text\tools\Capture2Text\Utils\tesseract\tesseract.exe'; // sesuaikan path ini sesuai dengan lokasi Tesseract di sistem Anda
 
-$result = (new TesseractOCR('kk.png'))
+$result = (new TesseractOCR('ridho.jpeg'))
     ->executable($tesseractPath) // arahkan ke executable Tesseract
     ->lang('ind')
-    
+    ->psm(6) // Menambahkan Page Segmentation Mode untuk meningkatkan akurasi
     ->run();
     echo $result;
